8d183b5f7c517de10b72da503651b3795690d2e9
Naravich Chantakanakakorn <naravit_out@hotmail.com>
2023-08-25 09:45:40 +0700

    [#04] - add span attributes

--- ch04/shopper.py
+++ ch04/shopper.py
@@ -2,35 +2,56 @@
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.semconv.trace import HttpFlavorValues, SpanAttributes
 from opentelemetry import trace
+from local_machine_resource_detector import LocalMachineResourceDetector
 
 
-def configure_tracer():
+def configure_tracer(name, version):
     exporter = ConsoleSpanExporter()
     span_processor = BatchSpanProcessor(exporter)
-    resource = Resource.create(
-        {
-            "service.name": "shopper",
-            "service.version": "0.1.2",
-        }
+    local_resource = LocalMachineResourceDetector().detect()
+    resource = local_resource.merge(
+        Resource.create(
+            {
+                "service.name": name,
+                "service.version": version,
+            }
+        )
     )
     provider = TracerProvider(resource=resource)
     provider.add_span_processor(span_processor)
     trace.set_tracer_provider(provider)
-    return trace.get_tracer("shopper.py", "0.0.1")
+    return trace.get_tracer(name, version)
 
 
-tracer = configure_tracer()
+tracer = configure_tracer("shopper", "0.1.2")
 
 
 @tracer.start_as_current_span("browse")
 def browse():
     print("visiting the grocery store")
-    add_item_to_cart("orange")
+    span = trace.get_current_span()
+    span.set_attributes(
+        {
+            SpanAttributes.HTTP_METHOD: "GET",
+            SpanAttributes.HTTP_FLAVOR: HttpFlavorValues.HTTP_1_1.value,
+            SpanAttributes.HTTP_URL: "http://localhost:5000",
+            SpanAttributes.NET_PEER_IP: "127.0.0.1",
+        }
+    )
+    add_item_to_cart("orange", 5)
 
 
 @tracer.start_as_current_span("add item to cart")
-def add_item_to_cart(item):
+def add_item_to_cart(item, quantity):
+    span = trace.get_current_span()
+    span.set_attributes(
+        {
+            "item": item,
+            "quantity": quantity,
+        }
+    )
     print("add {} to cart".format(item))
 
 
